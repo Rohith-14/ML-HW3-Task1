be7f735dad80c3000a92dc40a4314cc96defec08
Rohith Neralla <46639848+Rohith-14@users.noreply.github.com>
2022-11-22 23:51:28 -0500

    Add files via upload

--- Task 1 - Q4.py
+++ Task 1 - Q4.py
@@ -2,7 +2,7 @@
 """
 Created on Sun Nov 20 09:02:11 2022
 
-@author: Lahari
+@author: Rohith
 """
 
 import numpy as np 
@@ -13,48 +13,6 @@ import kmeans as km
 dataset2=km.loadCSV('./Datasets/datam.csv')
 k = len(set([x[0] for x in dataset2]))
 print('k=',k)
-
-# # Q1 - Comparing the SSE's of Euclidean-K-means, Cosine-K-means, Jarcard-K-means
-# print('\n Q1 \n')
-# clustering_euclidian = km.kmeans(dataset2,k,dist_type='euclidian')
-# clustering_cosine = km.kmeans(dataset2,k,dist_type='cosine')
-# clustering_jaccard = km.kmeans(dataset2,k,dist_type='jaccard')
-
-
-# print('Euclidian: ', clustering_euclidian['withinss'])
-# print('Cosine: ', clustering_cosine['withinss'])
-# print('Jaccard: ', clustering_jaccard['withinss'])
-
-# # Q2 - Comparing the accuracies of Euclidean-K-means, Cosine-K-means, Jarcard-K-means
-# print('\n Q2 \n')
-# def accuracy(cluster):
-#     label_accuracy = list()
-#     for cluster in cluster['clusters']:
-#         labels = dict()
-#         for item in cluster:
-#             labels.setdefault(item[0],0)
-#             labels[item[0]] += 1
-#         vals = np.array(list(labels.values()))
-#         vals.sort()
-#         if len(vals) == 0:
-#             label_accuracy.append(0)
-#         else:
-#             label_accuracy.append(vals[-1]/sum(vals))
-#     label_accuracy = np.array(label_accuracy)
-#     return label_accuracy.mean()
-
-# print('Euclidian: ', accuracy(clustering_euclidian))
-# print('Cosine: ', accuracy(clustering_cosine))
-# print('Jaccard: ', accuracy(clustering_jaccard))
-
-# # Q3 - Set up the same stop criteria: “when there is no change in centroid position"
-# print(' \n Q3 \n')
-# clustering_euclidian = km.kmeans(dataset2,k,dist_type='euclidian',condition='centroid')
-# print('Euclidian: ', clustering_euclidian['iterations'])
-# clustering_cosine = km.kmeans(dataset2,k,dist_type='cosine',condition='centroid')
-# print('Cosine: ', clustering_cosine['iterations'])
-# clustering_jaccard = km.kmeans(dataset2,k,dist_type='jaccard',condition='centroid')
-# print('Jaccard: ', clustering_jaccard['iterations'])
 
 print(' \n Q4 \n')
 from time import time
